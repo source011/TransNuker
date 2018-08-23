3c89f25be85fb33725045a787ebd922623eb216f
Tommy <tommycarlsson@ymail.com>
2018-08-23 15:31:11 +0200

    Update
    Some tweaks here and there

--- discord/TransNuke_Send.py
+++ discord/TransNuke_Send.py
@@ -2,15 +2,16 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir))
 
-# Set arguments to variables
+# Vars
 namn=sys.argv[1]
 torrent=sys.argv[2]
 ratio=sys.argv[3]
 seedTid=sys.argv[4]
-
+uploaded=sys.argv[5]
+size=sys.argv[6]
 from discordWebhooks import Webhook, Attachment, Field
 
-url = "" # Discord API url
+url = "" # Discord webhook url
 wh = Webhook(url, " ", namn + " - Transmission Nuker")
 
 at = Attachment(author_name = " ", color = "#ADFF2F", title = "[Torrent deleted!]")
@@ -21,13 +22,12 @@ field = Field("Seeding time", seedTid, True)
 at.addField(field)
 field = Field("Ratio", ratio, True)
 at.addField(field)
+field = Field("Total size", size, True)
+at.addField(field)
+field = Field("Total uploaded", uploaded, True)
+at.addField(field)
 field = Field("Torrent name", torrent, False)
 at.addField(field)
 
 wh.addAttachment(at)
-
-# Another message type
-#at = Attachment(author_name = "", color = "#ADFF2F", title = "")
-#wh.addAttachment(at)
-
 wh.post()
